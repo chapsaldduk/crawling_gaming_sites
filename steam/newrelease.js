f98f1d38a289a23ec320f5131d1a1854167259af
chapsaldduk <koala031532@gmail.com>
2023-03-24 14:04:39 +0900

    modified: crawling user agent

--- steam/newrelease.js
+++ steam/newrelease.js
@@ -44,6 +44,11 @@ async function crawlingTopSeller(driver) {
 }
 
 async function main() {
+  const options = new chrome.Options();
+  options.addArguments(
+    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"
+  );
+
   const driver = await new Builder()
     .forBrowser("chrome")
     .setChromeOptions(
