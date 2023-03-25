6fe4cea6b8b83f3a33b2783f3db5d5b3e9e00ce7
chapsaldduck <koala031532@gmail.com>
2023-03-26 00:34:31 +0900

    created: mariadb connect

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // server.js
 import express from "express";
+import maria from "./mariadb.js";
+maria.connect(); // db connect
 const app = express();
 
 const port = 8080;
@@ -11,4 +13,15 @@ app.listen(port, function () {
 app.get("/", function (req, res) {
   res.send("hello server");
   console.log(`localhost:${port}`);
+});
+
+app.get("/game", async function (req, res) {
+  maria.query("select * from main", function (err, rows, fields) {
+    if (!err) {
+      console.log("success");
+      res.send(rows);
+    } else {
+      console.log("connect error");
+    }
+  });
 });
